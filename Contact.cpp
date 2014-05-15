53147ac0378c12d408b1a0096e3b6330cc360511
Khabz <groutar@gmail.com>
2014-05-15 18:20:50 +0100

    Create Contact.cpp

---
+++ Contact.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Contact.h"
+
+using namespace std;
+
+Contact::Contact(string name, string address, string email, int number)
+{
+	this->name = name;
+	this->address = address;
+	this->email = email;
+	this->number = number;
+}
+
+ostream & operator<<(ostream &os, Contact C) {
+
+	os << C.getName() << ", " << C.getAddress() << ", " << C.getEmail() << ", " << C.getNumber() << endl;
+	return os;
+}
