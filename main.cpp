fee5245e7af10da4181f9526fefa158094ff0610
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-05-29 10:53:12 +0100

    Removed Windows libraries in Main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
+#include <sstream>
+#include <stdlib.h>
+
 #include "Contact.h"
 #include "ContactsManager.h"
-#include <sstream>
-#include <conio.h>
-#include <windows.h>
-#include <stdlib.h>
 
 using namespace std;
 
@@ -14,26 +13,6 @@ int stringToInt(string str) {
 	int n;
 	istringstream(str) >> n;
 	return n;
-}
-
-void clearScreen()
-{
-    COORD upperLeftCorner = {0,0};
-    DWORD charsWritten;
-    DWORD conSize;
-    HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_SCREEN_BUFFER_INFO  csbi;
-
-    GetConsoleScreenBufferInfo(hCon, &csbi);
-    conSize = csbi.dwSize.X * csbi.dwSize.Y;
-
-    // fill with spaces
-    FillConsoleOutputCharacter(hCon, TEXT(' '), conSize, upperLeftCorner, &charsWritten);
-    GetConsoleScreenBufferInfo(hCon, &csbi);
-    FillConsoleOutputAttribute(hCon, csbi.wAttributes, conSize, upperLeftCorner, &charsWritten);
-
-    // cursor to upper left corner
-    SetConsoleCursorPosition(hCon, upperLeftCorner);
 }
 
 int main() {
